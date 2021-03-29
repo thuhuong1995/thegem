92e0d2951a66341622c3c9effb33c1b133bab961
GhostFury <thuhuongvnhubt@gmail.com>
2021-03-29 13:49:47 +0700

    upload first

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+    function showVersion() {
+        var submenu = document.getElementById("version-list");
+        if (submenu.style.display === "none") {
+            submenu.style.display = "block";
+        } else {
+            submenu.style.display = "none";
+        }
+      }
+
+      function showProin() {
+        var submenu = document.getElementById("proin-list");
+        if (submenu.style.display === "none") {
+            submenu.style.display = "block";
+        } else {
+            submenu.style.display = "none";
+        }
+      }
+      function showSecondmenu() {
+        var submenu = document.getElementById("second-menu");
+        if (submenu.style.display === "none") {
+            submenu.style.display = "block";
+        } else {
+            submenu.style.display = "none";
+        }
+      }
