ca5c02f91ee4b324f1d147d1dbf9ed3f9290b2a1
GhostFury <thuhuongvnhubt@gmail.com>
2021-04-01 09:09:40 +0700

    update fix ferture/v1 on 09.00 01/4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,35 @@
-    function showVersion() {
-        var submenu = document.getElementById("version-list");
-        if (submenu.style.display === "none") {
-            submenu.style.display = "block";
-        } else {
-            submenu.style.display = "none";
-        }
-      }
+function showVersion() {
+  var submenu = document.getElementById("version-list");
+  if (submenu.style.display === "none") {
+    submenu.style.display = "block";
+  } else {
+    submenu.style.display = "none";
+  }
+}
 
-      function showProin() {
-        var submenu = document.getElementById("proin-list");
-        if (submenu.style.display === "none") {
-            submenu.style.display = "block";
-        } else {
-            submenu.style.display = "none";
-        }
-      }
-      function showSecondmenu() {
-        var submenu = document.getElementById("second-menu");
-        if (submenu.style.display === "none") {
-            submenu.style.display = "block";
-        } else {
-            submenu.style.display = "none";
-        }
-      }
+function showProin() {
+  var submenu = document.getElementById("proin-list");
+  if (submenu.style.display === "none") {
+    submenu.style.display = "block";
+  } else {
+    submenu.style.display = "none";
+  }
+}
+function showSecondmenu() {
+  var submenu = document.getElementById("second-menu");
+  if (submenu.style.display === "none") {
+    submenu.style.display = "block";
+    $(".fa-chevron-down").toggleClass("show");
+  } else {
+    submenu.style.display = "none";
+    $(".fa-chevron-right").toggleClass("hidden");
+  }
+}
+
+$(document).ready(function () {
+  $(".header-bar__btn").click(function () {
+    $(this).toggleClass("open");
+    $(".header-tool").toggleClass("hidden");
+    $(".header-menu").toggleClass("show");
+  });
+});
